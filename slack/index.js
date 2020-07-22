88bff7d2e062d8c91ca181e21d654fcda437f91c
Ogbara Godwin <godwin.onisofien@andela.com>
2020-07-22 14:11:47 +0100

    add slack integration

---
+++ slack/index.js
@@ -0,0 +1,24 @@
+const { WebClient } = require('@slack/web-api');
+
+token = "xoxb-1255069385634-1255420246771-GBEV6iBUjRg86qXKfIaC24ET"
+
+const web = new WebClient(token);
+
+(async () => {
+ 
+  // Post a message to the channel, and await the result.
+  // Find more arguments and details of the response: https://api.slack.com/methods/chat.postMessage
+  try { 
+    const result = await web.chat.postMessage({
+      text: 'This works all I needed was the permission to write to public channels',
+      channel: "C017P7H4BQC",
+    });
+
+    // The result contains an identifier for the message, `ts`.
+    console.log(`Successfully send message ${result.ts} in conversation ${conversationId}`);
+  } catch (error) {
+    console.log({error}, error.data.response_metadata)
+  }
+ 
+
+})();
